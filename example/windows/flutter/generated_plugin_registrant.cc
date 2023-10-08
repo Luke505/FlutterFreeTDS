0d925110c620bb7e4550379f3cf47ae49e7fa309
Luca Ciampa <luca.ciampa@icloud.com>
2023-10-08 08:19:22 +0200

    ffi downgrade to 1.2.0

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <freetds/freetds_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  freetds_pluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("freetds_plugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  freetds
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
